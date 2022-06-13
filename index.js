2ccf519d2e04a1de7daa0ed43fb0dfa55ba90963
goodwyyn <godwin.mtuma@student.moringaschool.com>
2022-06-13 10:07:45 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(source, sought){
+    return source.filter(
+        (likeMatch) => likeMatch.toLowerCase() ===sought.toLowerCase()
+    )
+
+}
+function fuzzyMatch(source, searchString){
+    return source.filter(
+        (likeMatch) =>
+        likeMatch.toLowerCase().indexOf(searchString.toLowerCase()) ===0
+
+    )
+}
+
+function matchName(source, orderName){
+    return source.filter((record) => record.name === orderName)
+}
